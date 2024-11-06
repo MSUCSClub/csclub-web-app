7f9f73148c45fe97756dc9bc70adee58eee28202
Saif418 <sai.mk1009@gmail.com>
2024-11-06 16:27:49 -0600

    First updates

---
+++ app/About/page.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export default function About()
+{
+    return(
+        <html lang="eng">
+            <head>
+
+            </head>
+
+            <body>
+                <header className="text-center row-start-1 text-5xl font-bold my-4">
+                    About Page
+                </header>
+                
+            </body>
+    </html>
+    );
+    
+}
